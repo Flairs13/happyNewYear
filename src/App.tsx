48ee2dcbf99c4522f086d5379e23e3a58c8cf4db
AChaplygin <flairs13@gmail.com>
2024-12-29 00:28:32 +0300

    add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {MainComponent} from "./pages/main";
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
 
 function App() {
 
   return (
-    <>
+    <MantineProvider>
       <MainComponent/>
-    </>
+    </MantineProvider>
   )
 }
 
