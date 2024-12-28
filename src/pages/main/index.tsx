f58d380842a2e4b404b22bb7d3320994d723cf14
AChaplygin <flairs13@gmail.com>
2024-12-28 22:53:59 +0300

    add gh-pages

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background-color: red;
+`
+export const MainComponent = () => {
+  return (
+    <Container>
+      <div>privet</div>
+    </Container>
+  );
+};
