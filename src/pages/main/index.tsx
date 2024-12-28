3138dd7d524a8c2aa1257426c61ea133d6cb92c5
AChaplygin <flairs13@gmail.com>
2024-12-29 00:21:34 +0300

    add video

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components'
+import video from '../../assets/testVideo.mp4'
 
 const Container = styled.div`
-    background-color: red;
+`
+const Video = styled.video`
+    width: 100%;
+    height: 100%;
 `
 export const MainComponent = () => {
+
   return (
     <Container>
-      <div>privet АРИИИШШАААА</div>
-      <div>kak dela?</div>
+      <Video id="bannerVideo" muted autoPlay loop>
+        <source src={video} type="video/mp4"/>
+      </Video>
     </Container>
   );
 };
